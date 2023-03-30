41def0f5157d833b67142e583e2639091810136a
Natalia Fernandez <naicolsx@gmail.com>
2023-03-30 16:41:10 -0500

    base

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Col } from "antd";
+import Searcher from "./components/Searcher";
+import "./App.css";
+import PokemonList from "./components/PokemonList";
+
+function App() {
+  return (
+    <div className="App">
+      <Col span={8} offset={8} >
+        <Searcher />
+      </Col>
+      <PokemonList />
+    </div>
+  );
+}
+
+export default App;
